a8c4a6e367c8fb2c565f0eb5b781d571f996082c
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-01-18 23:01:06 +0530

    Debugged 1D parity check

--- error_detection/1d_parity_check.py
+++ error_detection/1d_parity_check.py
@@ -1,7 +1,9 @@
+from colorama import Fore
+
 def add_parity(data: str, parity: int) -> str:
     count_ones = data.count('1')
 
-    if (parity == 1 and count_ones % 2 == 1) or (parity == 0 and count_ones % 2 == 0):
+    if (parity == count_ones % 2):
         data += "0"
     else:
         data += "1"
@@ -12,8 +14,7 @@ def add_parity(data: str, parity: int) -> str:
 def check(data: str, parity: int) -> bool:
     count_ones = data[:-1].count('1')
 
-    if (parity == 1 and count_ones % 2 == 1 and data[-1] == "0") or \
-       (parity == 0 and count_ones % 2 == 0 and data[-1] == "0"):
+    if (add_parity(data=data[:-1], parity=parity) == data):
         return True
     else:
         return False
@@ -24,10 +25,16 @@ def main() -> None:
     parity = int(input("Enter the parity (0 = even & 1 = odd): "))
 
     rx_num = add_parity(tx_num, parity=parity)
-    received_data = input("Enter the received data: ")
-    print(f"Given Data: {tx_num}\nTransmitted Data: {rx_num}")
+    print("\nParity Bit:", rx_num[-1])
+    print("Data after parity:", rx_num)
 
-    print(f"1D Parity Check: {check(received_data.zfill(16), parity=parity)}")
+    received_data = input("\nEnter the received data: ")
+    print(f"\nGiven Data: {tx_num}\nTransmitted Data: {rx_num}")
+
+    if (check(received_data.zfill(16), parity=parity) == True):
+        print(Fore.GREEN + "The data recieved is Error-free.\n")
+    else:
+        print(Fore.RED + "The data recieved has an Error.\n")
 
     return None
 
