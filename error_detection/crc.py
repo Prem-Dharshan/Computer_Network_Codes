c64f814a7fae61d8df43555be9366efd3a2c5c74
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-01-18 23:53:36 +0530

    Debugged CRC

--- error_detection/crc.py
+++ error_detection/crc.py
@@ -15,25 +15,24 @@ def xor(op1, op2):
 
 def divide(dividend, divisor):
     n = len(divisor)
-    n_zeros = (len(divisor) - 1) * "0"
-    dividend += n_zeros
 
     op = dividend[0:n]
-    i = n
+    i = n - 1
 
     while i < len(dividend):
+
+        i += 1
+
         if op[0] == "0":
-            i += 1
             op = op[1:] + (dividend[i] if i < len(dividend) else "")
             continue
 
         op = xor(op, divisor)[1:]
-
-        i += 1
-        if i < len(dividend):
-            op += dividend[i]
-        else:
+        
+        if (i >= len(dividend)):
             break
+
+        op += dividend[i]
 
     return op
 
@@ -43,14 +42,21 @@ def main():
     divisor = input("Enter the divisor (binary): ")
     n = len(divisor)
 
+    n_zeros = (len(divisor) - 1) * "0"
+    dividend += n_zeros
+
     op = divide(dividend=dividend, divisor=divisor)
 
-    appended_dividend = dividend + op
+    appended_dividend = dividend[:-n+1] + op
     print("CRC bits:", Fore.GREEN + op)
     print("Appended dividend after CRC:", Fore.GREEN + appended_dividend)
 
     received_data = input("Enter the received data (binary): ")
-    if divide(dividend=received_data, divisor=divisor) == "000":
+
+    rem = divide(dividend=received_data, divisor=divisor)
+    print(rem)
+
+    if (rem == len(rem)*"0"):
         print(Fore.GREEN + "Received data is correct!")
     else:
         print(Fore.RED + "Received data is corrupted!")
