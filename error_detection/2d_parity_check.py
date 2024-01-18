a341e2b847e2d747dc9925bb9cb26b4858823358
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-01-19 00:08:15 +0530

    Debugged 2D parity check

--- error_detection/2d_parity_check.py
+++ error_detection/2d_parity_check.py
@@ -1,81 +1,75 @@
 from typing import List
-from colorama import Fore
+from colorama import Fore, Style, init
+
+init(autoreset=True)
 
 
 def add_parity_bit(data: str, parity: int) -> str:
-
-    sum: int = 0
-
-    for i in data:
-        if i == "1":
-            sum += 1
-
-    if (parity == 1):
-        if (sum % 2 == 1):
-            return "0"
-        else:
-            return "1"
+    if parity == 1:
+        return "0" if data.count("1") % 2 == 1 else "1"
     else:
-        if (sum % 2 == 0):
-            return "0"
-        else:
-            return "1"
+        return "0" if data.count("1") % 2 == 0 else "1"
 
 
-def add_parity(data1: str, data2: str) -> str:
-
+def add_parity(data1: str, data2: str, parity: int) -> str:
     result = ""
+    if parity == 1:
+        for i in range(len(data1)):
+            if data1[i] == data2[i]:
+                result += "1"
+            else:
+                result += "0"
+    else:
+        for i in range(len(data1)):
+            if data1[i] == data2[i]:
+                result += "0"
+            else:
+                result += "1"
+    return result
 
-    for i in range(len(data1)):
-        if (data1[i] == data2[i]):
-            result = "0" + result
-        else:
-            result = "1" + result
 
-    parity_val = "0" if result.count("1") % 2 == 0 else "1"
+def detect(data: str, parity: int) -> bool:
+    n = len(data) // 3
+    data1 = data[:n]
+    data2 = data[n:n+n]
+    par = data[n+n:]
 
-    return parity_val
+    result = add_parity(data1=data1, data2=data2, parity=parity)
 
-
-def detect(data1: str, data2: str, bit) -> bool:
-
-    result = ""
-
-    for i in range(len(data1)):
-        if (data1[i] == data2[i]):
-            result = "0" + result
-        else:
-            result = "1" + result
-
-    parity_val = "0" if result.count("1") % 2 == 0 else "1"
-
-    return True if parity_val == bit else False
+    return result == par
 
 
 def main() -> None:
-
-    data1: str = input("\nEnter the 1st data to be transmitted: ").zfill(8)
-    data2: str = input("\nEnter the 2nd data to be transmitted: ").zfill(8)
-    # print(data1, data2)
+    data1: str = input("\nEnter the 1st data to be transmitted: ")
+    data2: str = input("\nEnter the 2nd data to be transmitted: ")
 
     parity = int(input("Enter the parity (0 = even & 1 = odd): "))
 
     tx_num1: str = data1
     tx_num2: str = data2
 
-    tx_num1 = f"{tx_num1}{add_parity_bit(data=data1, parity=parity)}"
-    tx_num2 = f"{tx_num2}{add_parity_bit(data=data2, parity=parity)}"
-    print(f"Data to be Transmitted: {tx_num1}\n{tx_num2}")
+    parity_bit1 = add_parity_bit(data=data1, parity=parity)
+    parity_bit2 = add_parity_bit(data=data2, parity=parity)
 
-    parity_bit: str = add_parity(tx_num1[:-1], tx_num2[:-1])
-    tx_data = f"{tx_num1[:-1]}{tx_num2[:-1]}{parity_bit}"
+    tx_num1 += parity_bit1
+    tx_num2 += parity_bit2
+
+    print(f"Parity bit for Data1: {Fore.BLUE}{parity_bit1}{Style.RESET_ALL}")
+    print(f"Parity bit for Data2: {Fore.BLUE}{parity_bit2}{Style.RESET_ALL}")
+
+    print(f"Data to be Transmitted:\n\tTx1: {tx_num1}\n\tTx2: {tx_num2}\n")
+
+    parity_str: str = add_parity(tx_num1, tx_num2, parity=parity)
+    print(f"Parity bits for Data1 and Data2: {\
+          Fore.BLUE}{parity_str}{Style.RESET_ALL}")
+
+    tx_data = f"{tx_num1}{tx_num2}{parity_str}"
     print(f"Data to be Transmitted: {tx_data}")
 
-    rx_num1 = input("Enter the received data1: ")
-    rx_num2 = input("Enter the received data2: ")
+    rx_data = input("Enter the received data: ")
 
-    if detect(rx_num1, rx_num2, parity_bit) == True:
-        print("No error")
+    if detect(rx_data, parity) == True:
+        print(Fore.GREEN + "No error")
     else:
         print(Fore.RED + "Error")
 
